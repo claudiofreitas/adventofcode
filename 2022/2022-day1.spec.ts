033e98f158ee79648cf8d7577fb32838cef75ffa
Claudio Freitas <claudiofreitas3@gmail.com>
2022-12-01 23:34:33 +0900

    Solve 2022/1.2

--- 2022/2022-day1.spec.ts
+++ 2022/2022-day1.spec.ts
@@ -1,14 +1,25 @@
-import { solveMaxCalories } from './2022-day1';
+import { solveMaxCalories, solveTop3MaxCalories } from './2022-day1';
 import { createReadStream } from 'fs';
 
-it('should solve for sample', async () => {
-  const filepath = `${__dirname}/2022-day1-input-sample.txt`;
-  const fileStream = createReadStream(filepath);
+const SAMPLE_DATA_PATH = `${__dirname}/2022-day1-input-sample.txt`;
+const REAL_DATA_PATH = `${__dirname}/real-input.txt`;
+
+it('should calculate the total calories for the top 1 elf carrying the most calories (sample data)', async () => {
+  const fileStream = createReadStream(SAMPLE_DATA_PATH);
   expect(await solveMaxCalories(fileStream)).toStrictEqual(24000);
 });
 
-it('should solve real', async () => {
-  const filepath = `${__dirname}/real-input.txt`;
-  const fileStream = createReadStream(filepath);
+it('should calculate the total calories for the top 1 elf carrying the most calories (real data)', async () => {
+  const fileStream = createReadStream(REAL_DATA_PATH);
   expect(await solveMaxCalories(fileStream)).toStrictEqual(70116);
+});
+
+it('should calculate total calories for the top 3 elves carrying the most calories (sample data)', async () => {
+  const fileStream = createReadStream(SAMPLE_DATA_PATH);
+  expect(await solveTop3MaxCalories(fileStream)).toStrictEqual(45000);
+});
+
+it('should calculate total calories for the top 3 elves carrying the most calories (real data)', async () => {
+  const fileStream = createReadStream(REAL_DATA_PATH);
+  expect(await solveTop3MaxCalories(fileStream)).toStrictEqual(206582);
 });
--- 2022/2022-day1.ts
+++ 2022/2022-day1.ts
@@ -7,18 +7,52 @@ const solveMaxCalories = async (input: fs.ReadStream) => {
     crlfDelay: Infinity,
   });
 
-  const dwarfTotalCalories: number[] = [];
+  const totalCaloriesByElf: number[] = [];
   let accumulator = 0;
   for await (const line of readLineInterface) {
     if (line) {
       accumulator += Number(line);
     } else {
-      dwarfTotalCalories.push(accumulator);
+      totalCaloriesByElf.push(accumulator);
       accumulator = 0;
     }
   }
+  if (accumulator !== 0) {
+    totalCaloriesByElf.push(accumulator);
+  }
 
-  return Math.max(...dwarfTotalCalories);
+  const caloriesSortedDesc = [...totalCaloriesByElf].sort(
+    (caloriesElfA, caloriesElfB) => caloriesElfB - caloriesElfA
+  );
+
+  return caloriesSortedDesc[0];
 };
 
-export { solveMaxCalories };
+const solveTop3MaxCalories = async (input: fs.ReadStream) => {
+  const readLineInterface = readline.createInterface({
+    input: input,
+    crlfDelay: Infinity,
+  });
+
+  const totalCaloriesByElf: number[] = [];
+  let accumulator = 0;
+  for await (const line of readLineInterface) {
+    if (line) {
+      accumulator += Number(line);
+    } else {
+      totalCaloriesByElf.push(accumulator);
+      accumulator = 0;
+    }
+  }
+  if (accumulator !== 0) {
+    totalCaloriesByElf.push(accumulator);
+  }
+
+  const caloriesSortedDesc = [...totalCaloriesByElf].sort(
+    (caloriesElfA, caloriesElfB) => caloriesElfB - caloriesElfA
+  );
+
+  return caloriesSortedDesc[0] + caloriesSortedDesc[1] + caloriesSortedDesc[2];
+};
+
+export { solveMaxCalories, solveTop3MaxCalories };
