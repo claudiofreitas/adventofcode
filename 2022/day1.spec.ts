94b776d97eceb54813c68803c3413fb39cef293d
Claudio Freitas <claudiofreitas3@gmail.com>
2022-12-03 02:15:39 +0900

    Refactor: organize file names

--- 2022/day1.spec.ts
+++ 2022/day1.spec.ts
@@ -1,8 +1,8 @@
-import { solveMaxCalories, solveTop3MaxCalories } from './2022-day1';
+import { solveMaxCalories, solveTop3MaxCalories } from './day1';
 import { createReadStream } from 'fs';
 
-const SAMPLE_DATA_PATH = `${__dirname}/2022-day1-input-sample.txt`;
-const REAL_DATA_PATH = `${__dirname}/real-input.txt`;
+const SAMPLE_DATA_PATH = `${__dirname}/day1-input-sample.txt`;
+const REAL_DATA_PATH = `${__dirname}/day1-input-real.txt`;
 
 it('should calculate the total calories for the top 1 elf carrying the most calories (sample data)', async () => {
   const fileStream = createReadStream(SAMPLE_DATA_PATH);
