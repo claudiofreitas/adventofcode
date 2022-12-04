c475c1078b9a8256e691a80336058d88f906bf2c
Claudio Freitas <claudiofreitas3@gmail.com>
2022-12-05 02:49:07 +0900

    ts: Solve 2022/3.2

--- typescript/2022/day3.spec.ts
+++ typescript/2022/day3.spec.ts
@@ -1,14 +1,24 @@
-import { solvePart1 } from './day3';
+import { solvePart1, solvePart2 } from './day3';
 
 const SAMPLE_DATA_PATH = `${__dirname}/day3-input-sample.txt`;
 const REAL_DATA_PATH = `${__dirname}/day3-input-real.txt`;
 
-it('should calculate points for strategy (sample)', async () => {
+it('should solve part 1 (sample)', async () => {
   const filename = SAMPLE_DATA_PATH;
   expect(await solvePart1(filename)).toStrictEqual(157);
 });
 
-it('should calculate points for strategy (real)', async () => {
+it('should solve part 1 (real)', async () => {
   const filename = REAL_DATA_PATH;
   expect(await solvePart1(filename)).toStrictEqual(7597);
+});
+
+it('should solve part 2 (sample)', async () => {
+  const filename = SAMPLE_DATA_PATH;
+  expect(await solvePart2(filename)).toStrictEqual(70);
+});
+
+it('should solve part 2 (real)', async () => {
+  const filename = REAL_DATA_PATH;
+  expect(await solvePart2(filename)).toStrictEqual(2607);
 });
--- typescript/2022/day3.ts
+++ typescript/2022/day3.ts
@@ -13,26 +13,56 @@ const sumArray = (a: number[]): number => {
   return accumulator;
 };
 
+const getArrayIntersection = (array1: number[], array2: number[]): number[] => {
+  const intersection: number[] = [];
+  for (let i = 0; i < array2.length; i += 1) {
+    const elementArray2 = array2[i];
+    if (array1.includes(elementArray2)) {
+      intersection.push(elementArray2);
+    }
+  }
+  return intersection;
+};
+
 const solvePart1 = async (filename: string) => {
   const lines = await readLines(filename);
 
   const priorityForRucksack = [];
   lines.forEach((rucksack) => {
-    const hash: Record<string, boolean> = {};
-    let found = false;
-    for (let i = 0; i < rucksack.length / 2; i++) {
-      hash[rucksack[i]] = false;
-    }
-    for (let i = rucksack.length / 2; i < rucksack.length && !found; i++) {
-      if (rucksack[i] in hash) {
-        const p = priorityForLetter(rucksack[i]);
-        priorityForRucksack.push(p);
-        found = true;
-      }
-    }
+    if (rucksack === '') return;
+    const compartment1 = rucksack
+      .split('')
+      .slice(0, rucksack.length / 2)
+      .map(priorityForLetter);
+    const compartment2 = rucksack
+      .split('')
+      .slice(rucksack.length / 2, rucksack.length)
+      .map(priorityForLetter);
+    const intersection = getArrayIntersection(compartment1, compartment2);
+    priorityForRucksack.push(intersection[0]);
   });
 
   return sumArray(priorityForRucksack);
 };
 
-export { solvePart1 };
+const solvePart2 = async (filename: string) => {
+  const lines = await readLines(filename);
+
+  const groupPriorities: number[] = [];
+  for (let lineIndex = 0; lineIndex < lines.length; lineIndex += 3) {
+    const rucksack1 = lines[lineIndex + 0].split('').map(priorityForLetter);
+    const rucksack2 = lines[lineIndex + 1].split('').map(priorityForLetter);
+    const rucksack3 = lines[lineIndex + 2].split('').map(priorityForLetter);
+    const intersection1And2 = getArrayIntersection(rucksack1, rucksack2);
+    const intersection1And2And3 = getArrayIntersection(
+      intersection1And2,
+      rucksack3
+    );
+    const priority = intersection1And2And3[0];
+    groupPriorities.push(priority);
+  }
+
+  return sumArray(groupPriorities);
+};
+
+export { solvePart1, solvePart2 };
