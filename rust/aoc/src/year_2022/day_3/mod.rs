2059e1d1cc1a0f87d7cf3288007a0cdbba1eb6e2
Claudio Freitas <claudiofreitas3@gmail.com>
2022-12-20 02:36:17 +0900

    rust: refactor 2022.3.2

--- rust/aoc/src/year_2022/day_3/mod.rs
+++ rust/aoc/src/year_2022/day_3/mod.rs
@@ -26,22 +26,15 @@ pub fn solve_part_1(lines: &Vec<String>) -> u32 {
 }
 
 #[allow(dead_code)]
-pub fn solve_part_2(_lines: &Vec<String>) -> u32 {
-    let s = _lines
+pub fn solve_part_2(lines: &Vec<String>) -> u32 {
+    lines
         .chunks(3)
         .map(|three_rucksacks| {
-            let a = [
+            [
                 three_rucksacks[0].chars().collect::<HashSet<char>>(),
                 three_rucksacks[1].chars().collect::<HashSet<char>>(),
                 three_rucksacks[2].chars().collect::<HashSet<char>>(),
-            ];
-            // dbg!(print_type_of(&a));
-            a
-
-            // three_rucksacks
-            //     .iter()
-            //     .map(|rucksack| rucksack.chars().collect::<HashSet<char>>())
-            //     .collect::<Vec<HashSet<char>>>()
+            ]
         })
         .map(|[set0, set1, set2]| &(&set0 & &set1) & &set2)
         .map(|intersection| {
@@ -56,8 +49,7 @@ pub fn solve_part_2(_lines: &Vec<String>) -> u32 {
                 None => panic!("It was supposed to not be empty"),
             }
         })
-        .sum::<u32>();
-    s
+        .sum::<u32>()
 }
 
 #[cfg(test)]
